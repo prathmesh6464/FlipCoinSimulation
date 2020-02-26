d7e50e08ebc2573136a06faaa098b5be726cf82f
Prathamaesh <prathmesh6464@gmail.com>
2020-02-26 23:34:10 +0530

    MODIFIED : Double combination feature added.

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -13,12 +13,19 @@ read -p "Enter A Number To Flip The coin : " flipCoin
 #TAKING COUNTS IN DICTIONARY
 for((iter=0;iter<$flipCoin;iter++))
 do
-   randomValue=$((RANDOM%2))
-   if(($randomValue==1))
+   randomValue=$((RANDOM%4))
+	echo "random value : " $randomValue
+   if(($randomValue==0))
    then
-      flipCoinCount["Head"]=$(("${flipCoinCount["Head"]}"+1))
-   else
-      flipCoinCount["Tail"]=$(("${flipCoinCount["Tail"]}"+1))
+      flipCoinCount["HH"]=$(("${flipCoinCount["HH"]}"+1))
+   elif(($randomValue==1))
+   then
+      flipCoinCount["TT"]=$(("${flipCoinCount["TT"]}"+1))
+   elif(($randomValue==2)) 
+	then
+	flipCoinCount["HT"]=$(("${flipCoinCount["HT"]}"+1))
+	else
+	flipCoinCount["TH"]=$(("${flipCoinCount["TH"]}"+1))
    fi
 done
 
@@ -30,5 +37,8 @@ echo "Value Of Dictionary : " ${flipCoinCount[@]}
 
 
 #TAKING PERCENTAGE
-echo "Percentage Of Head : "`echo "${flipCoinCount["Head"]}"/$flipCoin*100 | bc -l`
-echo "Percentage of Tail : " `echo "${flipCoinCount["Tail"]}"/$flipCoin*100 | bc -l`
+echo "Percentage Of Head & Head : " `echo "${flipCoinCount["HH"]}"/$flipCoin*100 | bc -l`
+echo "Percentage Of Tail & Tail : " `echo "${flipCoinCount["TT"]}"/$flipCoin*100 | bc -l`
+echo "Percentage Of Head & Tail : " `echo "${flipCoinCount["HT"]}"/$flipCoin*100 | bc -l`
+echo "Percentage Of Tail & Head : " `echo "${flipCoinCount["TH"]}"/$flipCoin*100 | bc -l`
+
